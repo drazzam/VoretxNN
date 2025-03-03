d9f0d6db4e8e052f871939402fad568e128eb749
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2025-03-03 04:56:27 +0200

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+"""
+Vortex Neural Network Models.
+
+This subpackage provides complete model implementations based on vortex mathematics principles.
+"""
+
+from vortexnn.models.advanced_vortex_gnn import AdvancedVortexGNN
+
+__all__ = ['AdvancedVortexGNN']
